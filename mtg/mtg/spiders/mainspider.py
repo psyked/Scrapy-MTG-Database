fc2888b06cdcd1ef27fafa51a219bcb974f4067c
James Ford <jford@psyked.co.uk>
2012-07-27 22:27:27 +0100

    Working version of the spidering page content from separate pages.
    Which means, data is extracted from pages linked in the search results,
    not the search results page itself.

--- mtg/mtg/spiders/mainspider.py
+++ mtg/mtg/spiders/mainspider.py
@@ -15,8 +15,9 @@ class CardSpider(BaseSpider):
 	def getCardDetails(self, response):
 		hxs = HtmlXPathSelector(response)
 
-		item = CardItem()
-		item['cardname'] = card.select('//div[@id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_nameRow"]/div[@class="value"]/text()').extract()[0].strip()
+		item = response.request.meta['item']
+		#item = CardItem()
+		item['cardname'] = hxs.select('//div[@id="ctl00_ctl00_ctl00_MainContent_SubContent_SubContent_nameRow"]/div[@class="value"]/text()').extract()[0].strip()
 		#item['convertedmana'] = card.select('span[@class="convertedManaCost"]/text()').extract()
 		#item['types'] = card.select('span[@class="typeLine"]/text()').extract()[0].strip()
 		#item['cardtext'] = card.select('div[@class="rulesText"]/p/text()').extract()
@@ -42,9 +43,9 @@ class CardSpider(BaseSpider):
 		items = []
 
 		for card in cards:
-			result = Request("http://gatherer.wizards.com/" + card.select('span[@class="cardTitle"]/a/@href').extract()[0], self.getCardDetails)
-			items.append(result) 
-			#item = CardItem()
+			#result = Request("http://gatherer.wizards.com/" + card.select('span[@class="cardTitle"]/a/@href').extract()[0], self.getCardDetails)
+			#items.append(result) 
+			item = CardItem()
 			#item['cardname'] = card.select('span[@class="cardTitle"]/a/text()').extract()
 			#item['convertedmana'] = card.select('span[@class="convertedManaCost"]/text()').extract()
 			#item['types'] = card.select('span[@class="typeLine"]/text()').extract()[0].strip()
@@ -56,9 +57,10 @@ class CardSpider(BaseSpider):
 			#item['cardnumber'] = card.select('span[@class="cardTitle"]/a/text()').extract()
 			#item['artist'] = card.select('span[@class="cardTitle"]/a/text()').extract()
 			#item['rating'] = card.select('span[@class="cardTitle"]/a/text()').extract()
-			#item['link'] = "http://gatherer.wizards.com/" + card.select('span[@class="cardTitle"]/a/@href').extract()
+			item['link'] = "http://gatherer.wizards.com/Pages/Search/" + card.select('span[@class="cardTitle"]/a/@href').extract()[0]
 			#item['uid'] = card.select('span[@class="cardTitle"]/a/text()').extract()
-			#items.append(item)
+			items.append(item)
+			yield Request(item['link'], meta={'item':item}, callback=self.getCardDetails)
 
 		for item in items:
 			yield item
